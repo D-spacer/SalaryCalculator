823547f08f54db50fbde93dd887a8e139b51d1eb
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-19 02:31:48 +0300

    Added dotenv

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import sys
 import hh_jobs
 import superjobs
 import table_maker
+from dotenv import load_dotenv
 
 def main():
   try:
+    load_dotenv()
+    sj_api_key = os.environ['SJ_API_KEY']
     queries = ['python', 'java', 'C++']
     superjob = {}
     headhunter = {}
     for query in queries:
-      count, salary = superjobs.predict_rub_salary_sj(query)
+      count, salary = superjobs.predict_rub_salary_sj(query, sj_api_key)
       superjob[query] = {'Average salary': salary, 'Queries': count}
       count, salary = hh_jobs.predict_rub_salary_hh(query)
       headhunter[query] = {'Average salary': salary, 'Queries': count}
