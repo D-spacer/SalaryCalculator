6113d56badcb7e1b0289243d52033b5e6e6ace89
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-20 11:17:30 +0300

    Removed extra cycle

--- table_maker.py
+++ table_maker.py
@@ -1,12 +1,11 @@
 from terminaltables import AsciiTable
 
 def create_table(dictionary):
-  table_data = [['Язык', 'Ср. зарплата', 'Число записей']]
-  for i in range(len(dictionary)):
-    table_data.append([])
+  table_layout = [['Язык', 'Ср. зарплата', 'Число записей']]
   for index, language in enumerate(dictionary, start=1):
-    table_data[index].append(language)
-    table_data[index].append(dictionary[language]['Average salary'])
-    table_data[index].append(dictionary[language]['Queries'])
-  table = AsciiTable(table_data)
+    table_layout.append([])
+    table_layout[index].append(language)
+    table_layout[index].append(dictionary[language]['Average salary'])
+    table_layout[index].append(dictionary[language]['Queries'])
+  table = AsciiTable(table_layout)
   print(table.table)
