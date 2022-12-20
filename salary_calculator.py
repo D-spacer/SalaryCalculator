c8ee8499268a992761edebc9450d295b829ae51e
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-20 11:10:59 +0300

    removed is None

--- salary_calculator.py
+++ salary_calculator.py
@@ -1,8 +1,8 @@
 def predict_salary(salary_from, salary_to):
   salary = 0
-  if salary_from is not None and salary_to is not None:
+  if salary_from and salary_to:
     salary = (salary_from + salary_to) / 2
-  elif salary_from is not None and salary_to is None:
+  elif salary_from and not salary_to:
     salary = salary_from * 1.2
   else:
     salary = salary_to * 0.8
