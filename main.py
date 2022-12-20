8f167abd4e97296ac8831367d462fa8165cf02fa
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-20 11:19:55 +0300

    Changed variables

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ import table_maker
 from dotenv import load_dotenv
 
 def main():
-  try:
-    load_dotenv()
-    sj_api_key = os.environ['SJ_API_KEY']
-    queries = ['python', 'java', 'C++']
-    superjob = {}
-    headhunter = {}
-    for query in queries:
-      count, salary = superjobs.predict_rub_salary_sj(query, sj_api_key)
-      superjob[query] = {'Average salary': salary, 'Queries': count}
-      count, salary = hh_jobs.predict_rub_salary_hh(query)
-      headhunter[query] = {'Average salary': salary, 'Queries': count}
-    table_maker.create_table(superjob)
-    table_maker.create_table(headhunter)
-  except requests.exceptions.HTTPError:
-    sys.exit('Неверная ссылка')
-  
+  languages = ['python', 'java', 'C++']
+  superjob_result = {}
+  headhunter_result = {}
+  load_dotenv()
+  sj_api_key = os.environ['SJ_API_KEY']
+  for language in languages:
+    try:
+      count, salary = superjobs.predict_rub_salary_sj(language, sj_api_key)
+      superjob_result[language] = {'Average salary': salary, 'Queries': count}
+      count, salary = hh_jobs.predict_rub_salary_hh(language)
+      headhunter_result[language] = {'Average salary': salary, 'Queries': count}
+    except requests.exceptions.HTTPError:
+      sys.exit('Неверная ссылка')
+  table_maker.create_table(superjob_result)
+  table_maker.create_table(headhunter_result)
+    
   
 if __name__ == "__main__":
     main()
