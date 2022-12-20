ad047eb5e4c6e85cb4174485cd7ba86169a7392e
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-20 11:11:48 +0300

    Changed variables, added try/except

--- superjobs.py
+++ superjobs.py
@@ -1,20 +1,21 @@
 import requests
 import salary_calculator
 
-def predict_rub_salary_sj(query, sj_api_key, pages=1):
+def predict_rub_salary_sj(language, pages=1, city=4, results_per_page=100, category=48, allow_without_salary=1):
   comulative = 0
   count = 0
+  key = 'v3.h.4419416.9944a64c342f275a7d558137f9aa0a817a3262dd.6775a860540a8b7b854f5fb005b38b467653988d'
   for page in range(pages):
     header = {
-      'X-Api-App-Id': sj_api_key
+      'X-Api-App-Id': key
     }
     parameters = {
       "page": page,
-      "count": 100,
-      'town': 4,
-      'catalogues': 48,
-      'keyword': query,
-      'no_agreement': 1,
+      "count": results_per_page,
+      'town': city,
+      'catalogues': category,
+      'keyword': language,
+      'no_agreement': allow_without_salary,
       'currency': 'rub'
     }
     response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=header, params=parameters)
@@ -23,8 +24,8 @@ def predict_rub_salary_sj(query, sj_api_key, pages=1):
     for job in decoded_response['objects']:
       count += 1
       salary = salary_calculator.predict_salary(job["payment_from"], job["payment_to"])
-      # print(job["payment_from"], job["payment_to"])
-      # print(job['profession'], job['town']['title'], salary)
       comulative += salary
-      
-  return count, int(comulative / count)
+  try:    
+    return count, int(comulative / count)
+  except ZeroDivisionError:
+    print('По данному запросу не найдено вакансий, измените запрос')
