2a41c08796b35d21f4fd12cb98ead1146574ae17
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-21 12:08:28 +0300

    deleted key

--- superjobs.py
+++ superjobs.py
@@ -4,10 +4,9 @@ import salary_calculator
 def predict_rub_salary_sj(language, sj_api_key, pages=1, city=4, results_per_page=100, category=48, allow_without_salary=1):
   comulative = 0
   count = 0
-  key = 'v3.h.4419416.9944a64c342f275a7d558137f9aa0a817a3262dd.6775a860540a8b7b854f5fb005b38b467653988d'
   for page in range(pages):
     header = {
-      'X-Api-App-Id': key
+      'X-Api-App-Id': sj_api_key
     }
     parameters = {
       "page": page,
