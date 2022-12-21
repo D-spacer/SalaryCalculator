760e1019f8395316349eb6dbb204be67089ce3c2
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-21 12:24:00 +0300

    Added sum for all vacancies

--- table_maker.py
+++ table_maker.py
@@ -1,11 +1,14 @@
 from terminaltables import AsciiTable
 
 def create_table(dictionary):
+  summ = 0
   table_layout = [['Язык', 'Ср. зарплата', 'Число записей']]
   for index, language in enumerate(dictionary, start=1):
     table_layout.append([])
     table_layout[index].append(language)
     table_layout[index].append(dictionary[language]['Average salary'])
     table_layout[index].append(dictionary[language]['Queries'])
+    summ += dictionary[language]['Queries']
+  table_layout.append([f'Всего найдено вакансий: {summ}'])
   table = AsciiTable(table_layout)
   return table.table
