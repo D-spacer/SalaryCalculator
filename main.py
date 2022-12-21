bce4a98e8baaa9961fe951473323b172f7031f4e
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-21 12:05:17 +0300

    Changed variable names

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 
 def main():
   languages = ['python', 'java', 'C++']
-  superjob_result = {}
-  headhunter_result = {}
+  superjob_dict = {}
+  headhunter_dict = {}
   load_dotenv()
   sj_api_key = os.environ['SJ_API_KEY']
   for language in languages:
     try:
       count, salary = superjobs.predict_rub_salary_sj(language, sj_api_key)
-      superjob_result[language] = {'Average salary': salary, 'Queries': count}
+      superjob_dict[language] = {'Average salary': salary, 'Queries': count}
       count, salary = hh_jobs.predict_rub_salary_hh(language)
-      headhunter_result[language] = {'Average salary': salary, 'Queries': count}
+      headhunter_dict[language] = {'Average salary': salary, 'Queries': count}
     except requests.exceptions.HTTPError:
       sys.exit('Неверная ссылка')
   print(table_maker.create_table(superjob_result))
