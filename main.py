2b8862a5e630b84dfe6145d0be9429c6ea9c77c0
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-22 12:28:10 +0300

    Fixed variable names

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ from dotenv import load_dotenv
 
 def main():
   languages = ['python', 'java', 'C++']
-  superjob_dict = {}
-  headhunter_dict = {}
+  superjob_stats = {}
+  headhunter_stats = {}
   load_dotenv()
   sj_api_key = os.environ['SJ_API_KEY']
   for language in languages:
     try:
       count, salary = superjobs.predict_rub_salary_sj(language, sj_api_key)
-      superjob_dict[language] = {'Average salary': salary, 'Queries': count}
+      superjob_stats[language] = {'Average salary': salary, 'Queries': count}
       count, salary = hh_jobs.predict_rub_salary_hh(language)
-      headhunter_dict[language] = {'Average salary': salary, 'Queries': count}
+      headhunter_stats[language] = {'Average salary': salary, 'Queries': count}
     except requests.exceptions.HTTPError:
       sys.exit('Неверная ссылка')
     except ZeroDivisionError:
       print('По данному запросу не найдено вакансий, измените запрос')
-  print(table_maker.create_table(superjob_dict))
-  print(table_maker.create_table(headhunter_dict))
+  print(table_maker.create_table(superjob_stats))
+  print(table_maker.create_table(headhunter_stats))
     
   
 if __name__ == "__main__":
