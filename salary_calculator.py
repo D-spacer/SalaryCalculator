70bb6287434041ed773c1c118c363ceee91961a5
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-30 11:53:43 +0300

    Fixed indentation

--- salary_calculator.py
+++ salary_calculator.py
@@ -1,9 +1,9 @@
 def predict_salary(salary_from, salary_to):
-  salary = 0
-  if salary_from and salary_to:
-    salary = (salary_from + salary_to) / 2
-  elif salary_from and not salary_to:
-    salary = salary_from * 1.2
-  else:
-    salary = salary_to * 0.8
-  return salary
+    salary = 0
+    if salary_from and salary_to:
+      salary = (salary_from + salary_to) / 2
+    elif salary_from and not salary_to:
+      salary = salary_from * 1.2
+    else:
+      salary = salary_to * 0.8
+    return salary
