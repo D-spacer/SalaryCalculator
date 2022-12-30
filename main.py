1cb60e8cdeafe5fc89f8d0a607fc9b8f8a3d73c8
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-30 11:54:29 +0300

    Fixed indentation

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import table_maker
 from dotenv import load_dotenv
 
 def main():
-  languages = ['python', 'java', 'C++']
-  superjob_stats = {}
-  headhunter_stats = {}
-  load_dotenv()
-  sj_api_key = os.environ['SJ_API_KEY']
-  for language in languages:
-    try:
-      count, salary = superjobs.predict_rub_salary_sj(language, sj_api_key)
-      superjob_stats[language] = {'Average salary': salary, 'Queries': count}
-      count, salary = hh_jobs.predict_rub_salary_hh(language)
-      headhunter_stats[language] = {'Average salary': salary, 'Queries': count}
-    except requests.exceptions.HTTPError:
-      sys.exit('Неверная ссылка')
-    except ZeroDivisionError:
-      print('По данному запросу не найдено вакансий, измените запрос')
-  print('Вакансии Superjob')
-  print(table_maker.create_table(superjob_stats))
-  print('Вакансии HeadHunter')
-  print(table_maker.create_table(headhunter_stats))
+    languages = ['python', 'java', 'C++']
+    superjob_stats = {}
+    headhunter_stats = {}
+    load_dotenv()
+    sj_api_key = os.environ['SJ_API_KEY']
+    for language in languages:
+      try:
+        count, salary = superjobs.predict_rub_salary_sj(language, sj_api_key)
+        superjob_stats[language] = {'Average salary': salary, 'Queries': count}
+        count, salary = hh_jobs.predict_rub_salary_hh(language)
+        headhunter_stats[language] = {'Average salary': salary, 'Queries': count}
+      except requests.exceptions.HTTPError:
+        sys.exit('Неверная ссылка')
+      except ZeroDivisionError:
+        print('По данному запросу не найдено вакансий, измените запрос')
+    print('Вакансии Superjob')
+    print(table_maker.create_table(superjob_stats))
+    print('Вакансии HeadHunter')
+    print(table_maker.create_table(headhunter_stats))
     
   
 if __name__ == "__main__":
