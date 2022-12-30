0bd5f2389f23fd520760e8d277793988ea7f68a8
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-30 11:53:20 +0300

    Fixed indentation

--- superjobs.py
+++ superjobs.py
@@ -2,26 +2,26 @@ import requests
 import salary_calculator
 
 def predict_rub_salary_sj(language, sj_api_key, pages=1, city=4, results_per_page=100, category=48, allow_without_salary=1):
-  comulative = 0
-  count = 0
-  for page in range(pages):
-    header = {
-      'X-Api-App-Id': sj_api_key
-    }
-    parameters = {
-      "page": page,
-      "count": results_per_page,
-      'town': city,
-      'catalogues': category,
-      'keyword': language,
-      'no_agreement': allow_without_salary,
-      'currency': 'rub'
-    }
-    response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=header, params=parameters)
-    response.raise_for_status()
-    decoded_response = response.json()
-    for job in decoded_response['objects']:
-      count += 1
-      salary = salary_calculator.predict_salary(job["payment_from"], job["payment_to"])
-      comulative += salary 
-  return count, int(comulative / count)
+    comulative = 0
+    count = 0
+    for page in range(pages):
+      header = {
+        'X-Api-App-Id': sj_api_key
+      }
+      parameters = {
+        "page": page,
+        "count": results_per_page,
+        'town': city,
+        'catalogues': category,
+        'keyword': language,
+        'no_agreement': allow_without_salary,
+        'currency': 'rub'
+      }
+      response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=header, params=parameters)
+      response.raise_for_status()
+      decoded_response = response.json()
+      for job in decoded_response['objects']:
+        count += 1
+        salary = salary_calculator.predict_salary(job["payment_from"], job["payment_to"])
+        comulative += salary 
+    return count, int(comulative / count)
