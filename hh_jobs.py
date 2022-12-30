c2b813c1a1a6244264c293ddfe5ea08ad20efddd
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-30 11:54:59 +0300

    Fixed indentation

--- hh_jobs.py
+++ hh_jobs.py
@@ -2,22 +2,22 @@ import requests
 import salary_calculator
 
 def predict_rub_salary_hh(vacancy, pages=1, period=30, region=1, results_per_page=100):
-  count = 0
-  comulative = 0
-  for page in range(pages):
-    parameters = {
-      'text': vacancy,
-      'only_with_salary': 'true',
-      'period': period,
-      'area': region,
-      "per_page": results_per_page,
-      "page": page,
-    }
-    response = requests.get('https://api.hh.ru/vacancies', params=parameters)
-    response.raise_for_status()
-    decoded_response = response.json()
-    for job in decoded_response['items']:
-      count += 1
-      salary = salary_calculator.predict_salary(job['salary']['from'], job['salary']['to'])
-      comulative += salary  
-  return count, int(comulative / count)
+    count = 0
+    comulative = 0
+    for page in range(pages):
+      parameters = {
+        'text': vacancy,
+        'only_with_salary': 'true',
+        'period': period,
+        'area': region,
+        "per_page": results_per_page,
+        "page": page,
+      }
+      response = requests.get('https://api.hh.ru/vacancies', params=parameters)
+      response.raise_for_status()
+      decoded_response = response.json()
+      for job in decoded_response['items']:
+        count += 1
+        salary = salary_calculator.predict_salary(job['salary']['from'], job['salary']['to'])
+        comulative += salary  
+    return count, int(comulative / count)
