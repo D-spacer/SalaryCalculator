6d318579cb59b260a35c64a6576201c5801ebeae
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2023-01-22 19:22:31 +0300

    added Processed column

--- table_maker.py
+++ table_maker.py
@@ -2,12 +2,13 @@ from terminaltables import AsciiTable
 
 def create_table(initial_data):
     summ = 0
-    table_layout = [['Язык', 'Ср. зарплата', 'Число записей']]
+    table_layout = [['Язык', 'Ср. зарплата', 'Число записей', 'Обработано']]
     for index, language in enumerate(initial_data, start=1):
       table_layout.append([])
       table_layout[index].append(language)
       table_layout[index].append(initial_data[language]['Average salary'])
       table_layout[index].append(initial_data[language]['Queries'])
+      table_layout[index].append(initial_data[language]['Processed'])
       summ += initial_data[language]['Queries']
     table_layout.append([f'Всего найдено вакансий: {summ}'])
     table = AsciiTable(table_layout)
