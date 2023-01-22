aa89bcddec596b8ab3003df35c07227584760a12
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2023-01-22 19:08:30 +0300

    removed search with salary

--- superjobs.py
+++ superjobs.py
@@ -14,7 +14,6 @@ def predict_rub_salary_sj(language, sj_api_key, pages=1, city=4, results_per_pag
         'town': city,
         'catalogues': category,
         'keyword': language,
-        'no_agreement': allow_without_salary,
         'currency': 'rub'
       }
       response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=header, params=parameters)
@@ -22,7 +21,10 @@ def predict_rub_salary_sj(language, sj_api_key, pages=1, city=4, results_per_pag
       decoded_response = response.json()
       for job in decoded_response['objects']:
         count += 1
-        salary = salary_calculator.predict_salary(job["payment_from"], job["payment_to"])
-        comulative += salary
+        with_salary = 0
+        if job["payment_from"] or job["payment_to"]:
+            salary = salary_calculator.predict_salary(job["payment_from"], job["payment_to"])
+            with_salary += 1
+            comulative += salary
     if count != 0:
-        return count, int(comulative / count)
+        return count, with_salary, int(comulative / count)
