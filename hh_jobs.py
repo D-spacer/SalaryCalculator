13d10f3b13f7cb30ae1fcec512fc222aff1dcd30
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2023-01-22 19:06:58 +0300

    removed search with salary

--- hh_jobs.py
+++ hh_jobs.py
@@ -3,6 +3,7 @@ import salary_calculator
 
 def predict_rub_salary_hh(vacancy, pages=1, period=30, region=1, results_per_page=100):
     count = 0
+    with_salary = 0
     comulative = 0
     for page in range(pages):
       parameters = {
@@ -18,7 +19,9 @@ def predict_rub_salary_hh(vacancy, pages=1, period=30, region=1, results_per_pag
       decoded_response = response.json()
       for job in decoded_response['items']:
         count += 1
-        salary = salary_calculator.predict_salary(job['salary']['from'], job['salary']['to'])
-        comulative += salary  
+        if job['salary']['from'] or job['salary']['to:']:
+      	    salary = salary_calculator.predict_salary(job['salary']['from'], job['salary']['to'])
+		    with_salary += 1
+      	    comulative += salary
     if count != 0:
-        return count, int(comulative / count)
+        return count, with_salary, int(comulative / count)
